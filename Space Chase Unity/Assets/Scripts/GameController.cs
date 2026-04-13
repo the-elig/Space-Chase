6cca9cefc64d5eef652538c576640eb305e8661c
the-elig <elidg@uci.edu>
2026-04-13 13:56:24 -0700

    more gamecontroller logic

--- Space Chase Unity/Assets/Scripts/GameController.cs
+++ Space Chase Unity/Assets/Scripts/GameController.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public int _energy;
+    public int _energy; // add five at the beginning of each player turn
     public int _currentRoom; // can be 1 - 11
     public List<int> _damagedRooms; // can be 1 - 11
 
 
-    public void enemyTurn()
+    public void enemyTurn() // called with button 
     {
+        // disable player buttons
+        
+
+        // check for game over
+        if (_damagedRooms.Count >= 5) // 5 is arbitrary rn
+        {
+            playerLoss();
+        }
+
+
+        // damage player ship
         int ran = Random.Range(1, 12);
         while (_damagedRooms.Contains(ran))
         {
@@ -22,10 +34,28 @@ public class GameController : MonoBehaviour
         {
             Debug.Log("Enemy damaged room " + ran);
             _damagedRooms.Add(ran);
+
+            if (ran == _currentRoom)
+            {
+                // special event or whatever that occurs when you're in damaged room
+            }
         }
         else
         {
             Debug.Log("Enemy missed.");
         }
+
+        _energy += 5; // goes to player's turn
+    }
+
+
+    public void playerLoss()
+    {
+        Debug.Log("player has lost");
+    }
+    
+    public void playerWin()
+    {
+        Debug.Log("player has won");
     }
 }
