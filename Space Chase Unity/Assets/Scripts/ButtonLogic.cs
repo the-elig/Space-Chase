88a1a4c424ce9978c029f717c9560ffbb45a4c78
the-elig <elidg@uci.edu>
2026-04-14 19:35:01 -0700

    more button logic

--- Space Chase Unity/Assets/Scripts/ButtonLogic.cs
+++ Space Chase Unity/Assets/Scripts/ButtonLogic.cs
@@ -5,10 +5,23 @@ using UnityEngine.UI;
 
 public class ButtonLogic : MonoBehaviour
 {
+    [SerializeField] private GameController _gameController;
+
     [SerializeField] private Transform _shipMap;
+    // [SerializeField] private Transform _bridge;
+    // [SerializeField] private Transform _weapons;
+    // [SerializeField] private Transform _engine;
+    // [SerializeField] private Transform _shields;
+    // [SerializeField] private Transform _communications;
 
     public void clickedBridge()
     {
-        _shipMap.gameObject.SetActive(false);
+        int travelDistance = 0;
+
+        if (_gameController._energy - travelDistance > 0)
+        {
+            _shipMap.gameObject.SetActive(false);
+            // _bridge.gameObject.SetActive(true);
+        }
     }
 }
