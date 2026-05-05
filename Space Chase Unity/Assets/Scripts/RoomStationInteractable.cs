d96502395045eabafa7102193a81bfe1b152b271
jmmdan <jmmcdan1@uci.edu>
2026-05-05 16:21:14 -0700

    working on screen shake

--- Space Chase Unity/Assets/Scripts/RoomStationInteractable.cs
+++ Space Chase Unity/Assets/Scripts/RoomStationInteractable.cs
@@ -51,18 +51,27 @@ public class RoomStationInteractable : MonoBehaviour
         {
             canvas.UIBackground(true);
             if (cardPicker != null)
+            {
                 cardPicker.OpenCardPicker(); //for engine and shields rn
-            else if(cardUpgrader != null) {
+                m_MyAudioSource.Play();
+            }
+            else if (cardUpgrader != null)
+            {
                 Debug.Log("card upgrader station");
+                m_MyAudioSource.Play();
                 cardUpgrader.OpenCardUpgrader();
-                return; }
+
+                return;
+            }
             else if (station != null)
             {
                 station.SetActive(true);
                 m_MyAudioSource.Play();
                 RoomCardSlot slot = station.GetComponentInChildren<RoomCardSlot>();
-                if (slot != null) {
-                    slot.UpdateStationMessage(false); } //updates damaged message to no longer appear
+                if (slot != null)
+                {
+                    slot.UpdateStationMessage(false);
+                } //updates damaged message to no longer appear
             }
         }
     }
